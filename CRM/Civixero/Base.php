fdf00b9021f3f29004fc6637dc8669b3454e949f
Matthew Wire <mjw@mjwconsult.co.uk>
2023-06-02 19:58:11 +0100

    Tweaks to TrackingCategory and add API4 Xero.TrackingCategoryPull and TrackingCategoryAddOption and TrackingCategoryDeleteOption

--- CRM/Civixero/Base.php
+++ CRM/Civixero/Base.php
@@ -3,6 +3,7 @@
 use Civi\API\Event\PrepareEvent;
 use Civi\Xero\ConnectorInterface;
 use League\OAuth2\Client\Token\AccessToken;
+use XeroAPI\XeroPHP\Api\AccountingApi;
 
 /**
  * Class CRM_Civixero_Base
@@ -13,7 +14,10 @@ class CRM_Civixero_Base {
 
   private static array $singleton = [];
 
-  private $_xero_access_token;
+  /**
+   * @var \League\OAuth2\Client\Token\AccessToken
+   */
+  private AccessToken $_xero_access_token;
 
   private string $_xero_tenant_id;
 
@@ -266,20 +270,19 @@ class CRM_Civixero_Base {
    * @throws \CRM_Civixero_Exception_XeroThrottle
    */
   public static function checkApiRateExceeded(PrepareEvent $event): void {
-    if ($event->getEntityName() === 'Civixero' && (
-      substr($event->getActionName(), -4) === 'push'
-      || substr($event->getActionName(), -4) === 'pull'
-    )) {
-      $rateLimitExceeded = \Civi::settings()->get('xero_oauth_rate_exceeded');
-      if (!$rateLimitExceeded) {
-        return;
-      }
-      // Wait for 1 hour if rate limit was exceeded and then retry
-      if (strtotime('+1 hours', $rateLimitExceeded) > time()) {
-        throw new CRM_Civixero_Exception_XeroThrottle('Rate limit was previously triggered. Try again in 1 hour');
-      }
-      self::resetApiRateLimitExceeded();
+    // API3: Civixero; API4: Xero
+    if (!in_array($event->getEntityName(), ['Civixero', 'Xero'])) {
+      return;
     }
+    $rateLimitExceeded = \Civi::settings()->get('xero_oauth_rate_exceeded');
+    if (!$rateLimitExceeded) {
+      return;
+    }
+    // Wait for 1 hour if rate limit was exceeded and then retry
+    if (strtotime('+1 hours', $rateLimitExceeded) > time()) {
+      throw new CRM_Civixero_Exception_XeroThrottle('Rate limit was previously triggered. Try again in 1 hour');
+    }
+    self::resetApiRateLimitExceeded();
   }
 
   /**
